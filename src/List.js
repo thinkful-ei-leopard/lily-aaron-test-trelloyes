a4c25b6042f4ce33eab1eaf87c06deaf83e272d0
shimmy77 <christianshim@gmail.com>
2020-01-27 11:43:31 -0600

    initial commit

---
+++ src/List.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './Card';
+
+function List({header, cards}) {
+    return (
+       <section className="List">
+           
+       </section>
+    )
+}

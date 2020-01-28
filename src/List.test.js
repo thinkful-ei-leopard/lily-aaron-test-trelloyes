117b7a24b14de0df547d0e88384ffd2fd1ab6652
Lillian Burnside <lillian.burnside@gmail.com>
2020-01-28 11:32:59 -0600

    finished list tests

--- src/List.test.js
+++ src/List.test.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import List from '/.List';
+import List from './List';
 
 //smoke test
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<List />, div);
+    ReactDOM.render(
+      <List 
+      cards = {[
+        {id: 'z',
+        title: 'something',
+        content: 'lorem ipsum'}
+      ]}
+      />, 
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
 //snapshot test
 
 it('renders UI as expected', () => {
-  const tree = renderer.create(<List />).toJSON();
+  const tree = renderer.create(
+    <List 
+      cards = {[
+        {id: 'z',
+        title: 'something',
+        content: 'lorem ipsum'}
+      ]}
+    />).toJSON();
   expect(tree).toMatchSnapshot();
 });

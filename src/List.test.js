d5241a53c57df9c6080a1c58029f1c4442f7689e
Aaron Turcios <aar.turcios@gmail.com>
2020-01-28 09:00:36 -0800

    added testing to list

--- src/List.test.js
+++ src/List.test.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import List from '/.List';
+
 //smoke test
 
-
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
 //snapshot test
+
+it('renders UI as expected', () => {
+  const tree = renderer.create(<List />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
